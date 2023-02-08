aa1513cd0d71d442084d8c44615fbc5fc019ee37
ellipse12 <ellipse1234567@gmail.com>
2023-02-08 13:43:44 -0700

    start

---
+++ src/main/java/Engine/DisplayManager.java
@@ -0,0 +1,46 @@
+package Engine;
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class DisplayManager {
+    public  long create(){
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        long window = glfwCreateWindow(300, 300, "test", NULL, NULL);
+        if(window == NULL){
+            throw new RuntimeException("Unable to create window");
+        }
+        glfwSetKeyCallback(window, (win, key, scancode, action, mods)->{
+            if(key==GLFW_KEY_ESCAPE&&action==GLFW_RELEASE){
+                glfwSetWindowShouldClose(window, true);
+            }
+        });
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+            glfwGetWindowSize(window, pWidth, pHeight);
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+        glfwShowWindow(window);
+        return window;
+
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,36 @@
+import Engine.DisplayManager;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+
 public class Main {
+
+        public static void main(String[] args){
+            Main main = new Main();
+            main.init();
+            long window = new DisplayManager().create();
+            glClearColor(0.0f,0.0f,1.0f, 0.0f);
+            while(glfwWindowShouldClose(window)){
+                main.loop(window);
+            }
+
+        }
+        public  void init(){
+            GLFWErrorCallback.createPrint(System.err).set();
+            if ( !glfwInit() ) {
+                throw new IllegalStateException("Unable to initialize GLFW");
+            }
+            GL.createCapabilities();
+        }
+        public  void loop(long window){
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+                glfwSwapBuffers(window);
+
+                glfwPollEvents();
+        }
+
 }
